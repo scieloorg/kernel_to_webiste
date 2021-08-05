a35c51137e0b4f3cce3233e29f5fc8ff82cdc905
Rafael JPD <rafael.pezzuto@gmail.com>
2021-08-05 19:30:57 -0300

    adiciona urls relacionadas aos modelos novos

--- spf/core/urls.py
+++ spf/core/urls.py
@@ -3,11 +3,15 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.home_page, name='home'),
-    path('register/', views.register_page, name='register'),
-    path('login/', views.login_page, name='login'),
+    path('', views.index_page, name='index'),
+    path('register/', views.account_register_page, name='register'),
+    path('login/', views.account_login_page, name='login'),
     path('logout/', views.logout_user, name='logout'),
-    path('user/', views.user_page, name='user'),
-    path('journals/', views.journals_page, name='journals'),
-    path('create/', views.create_user_page, name='create_user'),
+    path('package/list', views.DepositedPackagesListView.as_view(), name='package_list'),
+    path('journal/list', views.journal_list_page, name='journal_list'),
+    path('user/add', views.user_add_page, name='user_add'),
+    path('user/dashboard', views.user_dashboard_page, name='user_dashboard'),
+    path('user/package/list', views.DepositedPackagesByUserListView.as_view(), name='user_package_list'),
+    path('user/profile', views.user_profile_page, name='user_profile'),
+    path('user/profile/edit', views.UserEditView.as_view(), name='user_profile_edit'),
 ]

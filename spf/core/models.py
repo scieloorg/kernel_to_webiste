9555a36faa1c71eb0c3914e4418ed669ba7ddc97
Rafael JPD <rafael.pezzuto@gmail.com>
2021-08-05 19:28:36 -0300

    adiciona modelos básicos relacionados a deposito, pacote e validação

--- spf/core/models.py
+++ spf/core/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Journal(models.Model):
@@ -9,3 +10,37 @@ class Journal(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Package(models.Model):
+    name = models.CharField(max_length=200)
+    deposit_date = models.DateTimeField(auto_now=True)
+    state = models.IntegerField(default=0)
+    depositor = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+
+
+class Document(models.Model):
+    name = models.CharField(max_length=200)
+    package = models.ForeignKey(Package, on_delete=models.CASCADE)
+    journal = models.ForeignKey(Journal, on_delete=models.CASCADE)
+
+
+class DocumentFile(models.Model):
+    name = models.CharField(max_length=200)
+    document = models.ForeignKey(Document, on_delete=models.CASCADE)
+    file = models.FileField()
+
+
+class DocumentAsset(models.Model):
+    name = models.CharField(max_length=200)
+    document = models.ForeignKey(Document, on_delete=models.CASCADE)
+    file = models.FileField()
+
+
+class ValidationSchema(models.Model):
+    journal = models.ForeignKey(Journal, on_delete=models.PROTECT)
+
+
+class Validation(models.Model):
+    name = models.CharField(max_length=200)
+    validation_schema = models.ForeignKey(ValidationSchema, on_delete=models.PROTECT)

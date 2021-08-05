ada5b8d0d17e51b3482da0fafd0f69e864b9d679
Rafael JPD <rafael.pezzuto@gmail.com>
2021-08-05 19:27:47 -0300

    registra entidades novas no admin

--- spf/core/admin.py
+++ spf/core/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
-from .models import Journal
+from .models import Journal, Package, Document, DocumentFile, DocumentAsset, ValidationSchema, Validation
 
 
 admin.site.register(Journal)
+admin.site.register(Package)
+admin.site.register(Document)
+admin.site.register(DocumentFile)
+admin.site.register(DocumentAsset)
+admin.site.register(ValidationSchema)
+admin.site.register(Validation)

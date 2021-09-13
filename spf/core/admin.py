5895f898d5a88c0bfbf2a8c36c908c21d0578fea
Rafael JPD <rafael.pezzuto@gmail.com>
2021-09-12 21:27:35 -0300

    atualiza admin com modelos novos

--- spf/core/admin.py
+++ spf/core/admin.py
@@ -1,11 +1,6 @@
 from django.contrib import admin
-from .models import Journal, Package, Document, DocumentFile, DocumentAsset, ValidationSchema, Validation
+from .models import Event, ValidationSchema, Validation
 
-
-admin.site.register(Journal)
-admin.site.register(Package)
-admin.site.register(Document)
-admin.site.register(DocumentFile)
-admin.site.register(DocumentAsset)
+admin.site.register(Event)
 admin.site.register(ValidationSchema)
 admin.site.register(Validation)

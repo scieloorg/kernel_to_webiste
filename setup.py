3e2333c5cf0fe829d12e6dedb134dc812cf379b7
Rafael J P Damaceno <rafael.pezzuto@gmail.com>
2021-09-18 15:18:51 -0300

    adiciona setup.py

---
+++ setup.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+import os, setuptools
+
+here = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(here, "README.md")) as f:
+    README = f.read()
+
+requires = [
+    'asgiref',
+    'confusable-homoglyphs',
+    'Django',
+    'pytz',
+    'python-dateutil',
+    'sqlparse',
+    'minio',
+    'opac_schema',
+    'dsm',
+    'scielo_v3_manager',
+]
+
+tests_require = [
+]
+
+setuptools.setup(
+    name="SciELO Publishing Framework",
+    version="0.1",
+    author="SciELO",
+    author_email="scielo-dev@googlegroups.com",
+    description="",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    license="2-clause BSD",
+    packages=setuptools.find_packages(
+        exclude=["*.tests", "*.tests.*", "tests.*", "tests"]
+    ),
+    include_package_data=True,
+    extras_require={"testing": tests_require},
+    install_requires=requires,
+    dependency_links=[
+    ],
+    python_requires=">=3.6",
+    test_suite="tests",
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Environment :: Other Environment",
+        "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3 :: Only",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
+        "Operating System :: OS Independent",
+    ],
+)

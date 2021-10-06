a8daad01a3359ceaed7368d2021b76cf787e5b50
Rafael JPD <rafael.pezzuto@gmail.com>
2021-10-06 17:27:02 -0300

    inclui IngressPackage na página administrativa do Django

--- spf/core/admin.py
+++ spf/core/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Event, ValidationSchema, Validation
+from .models import Event, IngressPackage, ValidationSchema, Validation
 
 admin.site.register(Event)
+admin.site.register(IngressPackage)
 admin.site.register(ValidationSchema)
 admin.site.register(Validation)

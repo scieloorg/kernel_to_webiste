e43542147c172720fa9f07002a24e0c2fe8f56cf
Rafael J P D <rafael.pezzuto@gmail.com>
2021-11-01 21:56:43 -0300

    adiciona base do app core

---
+++ app/core/controller.py
@@ -0,0 +1,87 @@
+from datetime import datetime
+from django.contrib.auth.models import Group, User
+from core.models import GROUP_MANAGER, SCOPE_ALL_USERS, IngressPackage, Event
+
+
+def _is_privileged_user(user):
+    return GROUP_MANAGER in get_groups_names_from_user(user) or user.is_superuser or user.is_staff
+
+
+def _get_objects_from_user_and_scope(user, scope, model_class):
+    if scope == SCOPE_ALL_USERS and _is_privileged_user(user):
+        return model_class.objects.all()
+    else:
+        return model_class.objects.filter(user=user)
+
+
+def get_ingress_packages_from_user_and_scope(user, scope):
+    return _get_objects_from_user_and_scope(user, scope, IngressPackage)
+
+
+def get_events_from_user_and_scope(user, scope):
+    return _get_objects_from_user_and_scope(user, scope, Event)
+
+
+def get_user_from_username(username):
+    return User.objects.get(username=username)
+
+
+def get_users(include_superuser=False):
+    return User.objects.filter(is_superuser=include_superuser)
+
+
+def get_groups_names_from_user(user):
+    return [g.name for g in user.groups.all()]
+
+
+def get_groups_from_user(user):
+    return Group.objects.filter(user=user)
+
+
+def update_user_groups(user, user_groups):
+    for group in get_groups():
+        if group not in user_groups:
+            user.groups.remove(group)
+
+    for ug in user_groups:
+        user.groups.add(ug)
+        user.save()
+
+
+def get_groups_from_groups_names(groups_names):
+    return Group.objects.filter(name__in=groups_names)
+
+
+def get_groups():
+    return Group.objects.all()
+
+
+def add_event(user, event_name, annotation=None):
+    event = Event()
+    event.user = user
+    event.name = event_name
+    event.annotation = annotation
+    event.status = Event.Status.INITIATED
+    event.save()
+
+    return event
+
+
+def update_event(event, args):
+    for k, v in args.items():
+        setattr(event, k, v)
+    event.datetime = datetime.utcnow()
+    event.save()
+
+    return event
+
+
+def add_ingress_package(user, event_datetime, package_name):
+    ip = IngressPackage()
+    ip.user = user
+    ip.datetime = event_datetime
+    ip.package_name = package_name
+    ip.status = IngressPackage.Status.RECEIVED
+    ip.save()
+
+    return ip
---
+++ app/core/urls.py
@@ -0,0 +1,37 @@
+from django.urls import path
+from . import views
+
+
+general = [
+    path('', views.index_page, name='index'),
+    path('faq/', views.faq_page, name='faq'),
+]
+
+user = [
+    path('register/', views.user_register_page, name='register'),
+    path('login/', views.user_login_page, name='login'),
+    path('logout/', views.user_logout, name='logout'),
+    path('user/add/', views.user_add_page, name='user_add'),
+    path('user/change_password/', views.user_change_password_page, name='user_change_password'),
+    path('user/groups/edit', views.user_groups_edit_page, name='user_groups_edit'),
+    path('user/profile/', views.user_profile_page, name='user_profile'),
+    path('user/profile/edit/', views.user_profile_edit_page, name='user_profile_edit'),
+]
+
+ingress = [
+    path('ingress/package_upload/', views.ingress_package_upload_page, name='ingress_package_upload'),
+    path('ingress/package_download/', views.ingress_package_download_page, name='ingress_package_download'),
+    path('ingress/package_list/', views.ingress_package_list_page, name='ingress_package_list'),
+    path('ingress/articles_files/', views.ingress_articles_files_page, name='ingress_article_files_list'),
+]
+
+journal = [
+    path('journal/list/', views.journal_list_page, name='journal_list'),
+]
+
+tracking = [
+    path('event/list/', views.event_list_page, name='event_list'),
+    path('task/update_status/', views.task_update_status, name='task_update_status'),
+]
+
+urlpatterns = general + user + ingress + journal +  tracking

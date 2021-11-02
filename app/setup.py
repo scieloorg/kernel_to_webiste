05ec61913203bd59f6c9e5b9e2eeec0dbdf0841d
Rafael J P D <rafael.pezzuto@gmail.com>
2021-11-01 22:44:11 -0300

    melhora setup.py

--- app/setup.py
+++ app/setup.py
@@ -1,10 +1,6 @@
 #!/usr/bin/env python3
 import os, setuptools
 
-here = os.path.abspath(os.path.dirname(__file__))
-with open(os.path.join(here, "README.md")) as f:
-    README = f.read()
-
 requires = [
     'asgiref',
     'celery',
@@ -30,9 +26,7 @@ setuptools.setup(
     version="0.1",
     author="SciELO",
     author_email="scielo-dev@googlegroups.com",
-    description="",
-    long_description=README,
-    long_description_content_type="text/markdown",
+    description="This is the SciELO Upload Framework",
     license="2-clause BSD",
     packages=setuptools.find_packages(
         exclude=["*.tests", "*.tests.*", "tests.*", "tests"]
@@ -44,15 +38,4 @@ setuptools.setup(
     ],
     python_requires=">=3.6",
     test_suite="tests",
-    classifiers=[
-        "Development Status :: 2 - Pre-Alpha",
-        "Environment :: Other Environment",
-        "License :: OSI Approved :: BSD License",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3 :: Only",
-        "Topic :: Internet :: WWW/HTTP",
-        "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
-        "Operating System :: OS Independent",
-    ],
 )

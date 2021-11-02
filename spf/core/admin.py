48bd6480172acd89f3f053896be0a096d66c45ea
Rafael J P D <rafael.pezzuto@gmail.com>
2021-11-01 21:52:46 -0300

    remove arquivos e estrutura legada

--- spf/core/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Event, IngressPackage, ValidationSchema, Validation
-
-admin.site.register(Event)
-admin.site.register(IngressPackage)
-admin.site.register(ValidationSchema)
-admin.site.register(Validation)
--- spf/core/management/commands/make_messages_no_fuzzy.py
+++
@@ -1,5 +0,0 @@
-from django.core.management.commands import makemessages
-
-
-class Command(makemessages.Command):
-    msgmerge_options = ['-q', '--previous', '--no-fuzzy-matching']

9fbe7100f540d4aa17839515a150e34cb6070060
Rafael JPD <rafael.pezzuto@gmail.com>
2021-12-29 13:41:50 -0300

    adequa task para coleta de pacotes

--- app/core/tasks.py
+++ app/core/tasks.py
@@ -12,34 +12,17 @@ def task_get_package_uri_by_pid(self, pid, user_id):
     user = controller.get_user_from_id(user_id)
 
     # cria evento de pesquisa por pacote
-    ev = controller.add_event(
-        user,
-        Event.Name.RETRIEVE_PACKAGE,
-        annotation={
-            'pid': pid,
-        }
-    )
+    ev = controller.add_event(user, Event.Name.RETRIEVE_PACKAGE, annotation={'pid': pid})
 
     # obtém o {uri, name} de todos os pacotes existentes para o PID informado
     result = dsm_ingress.get_package_uri_by_pid(pid)
 
-    app.current_task.update_state(
-        state='PROGRESS',
-        meta={
-            'status': 'LOADING...',
-        })
+    app.current_task.update_state(state='PROGRESS', meta={'status': 'LOADING...',})
 
     if result['errors']:
         # houve alguma falha. atualiza status com valor FAILED e conteúdo da falha ocorrida
-        controller.update_event(
-            ev,
-            {
-                'status': Event.Status.FAILED,
-                'annotation': result,
-            }
-        )
+        controller.update_event(ev, {'status': Event.Status.FAILED, 'annotation': result,})
     else:
-
         # evento ocorreu com sucesso. atualiza status com valor COMPLETED
         controller.update_event(ev, {'status': Event.Status.COMPLETED})
 
